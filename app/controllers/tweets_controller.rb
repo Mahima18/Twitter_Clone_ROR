1df6b44248db719232994365bd90ab40920d050e
Mahima <mahimapatel54@gmail.com>
2021-04-17 01:28:58 +0530

    added tweet authorization for authorized user

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,25 @@
 class TweetsController < ApplicationController
+	before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+	before_action :correct_user, only: [:show, :edit, :destroy]
 	before_action :authenticate_user!
 
 	def index
 	  @tweets = Tweet.all
 	end
+
+	def show
+	end
 	 
 	def new
-	  @tweet = Tweet.new
+	  @tweet = current_user.tweets.build
+	end
+
+	def edit
 	end
 	 
 	def create
-	  @tweet = Tweet.new(tweet_params)
+	  @tweet = current_user.tweets.build(tweet_params)
+
 	  @tweet.user_id = current_user.id
 	  if @tweet.save
 	      redirect_to '/tweets#index'
@@ -18,8 +27,33 @@ class TweetsController < ApplicationController
 	    render 'new'
 	  end
 	end
+
+	def update
+		respond_to do |format|
+			if @tweet.update(tweet_params)
+				format.html { redirect_to @tweet, notice: 'Blog was successfully updated.' }
+			else
+				format.html { render :edit }
+			end
+		end
+	end
+
+	def destroy
+		@tweet = Tweet.find(params[:id])
+		@tweet.destroy
+		redirect_to '/', :notice => "Your tweet has been deleted"
+	end
+
+	def correct_user
+		@tweet = current_user.tweets.find_by(id: params[:id])
+		redirect_to root_path, notice: "not authorized" if @tweet.nil?
+	end
 	 
 	private
+
+	def set_tweet
+		@tweet = Tweet.find(params[:id])
+	end
 	  
 	def tweet_params
 	  params.require(:tweet).permit(:body, :title)
--- config/initializers/social_share_button.rb
+++
@@ -1,3 +0,0 @@
-SocialShareButton.configure do |config|
-  config.allow_sites = %w(facebook email linkedin whatsapp_app)
-end
